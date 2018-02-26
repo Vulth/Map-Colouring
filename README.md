4312704b70e4e66d56581909e614073b83e12a2f
Unknown <thomasloughton@hotmail.ca>
2018-02-26 14:18:28 -0800

    Readme updated, comments added

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Map-Colouring
 ## A program written in Python that will assign colours to a set of connected nodes such that no adjacent nodes will have the same colour
+
+### Program Design:
+
+The program starts by calling solve, which will allocate two arrays 
+Map_array keeps track of each node and the colour it has been assigned.
+Map_array[1] = 2 indicates that node 1 has been assigned the colour 2.
+The 0th index is unused.
+If a node has the value 0, that means it has not been assigned a colour yet.
+Colour_array is a list of lists, where colour_array[1] is the list of colours that node 1 can be legally assigned.
+
+After allocating these two lists, the recursive backtracking search function is called.
+
+Backtracking will first check if the Map_array is completely filled. If it is, it will return Map Array as a valid solution to the nodes. If it is not completely filled, it will call selectNextNode to determine the best node to assign a colour to.
+
+SelectNextNode uses the minimum remaining values and degree heuristics to determine which node is optimal for solving the node puzzle. If SelectNextNode returns a null, the current colour assignments are unviable. The program will go back a step and try the next legal colour. Once a node has been chosen by SelectNextNode, LeastConstrainingValue will be used to create a list of best to worst colour choices for the chosen node.
+
+LeastConstrainingValue will return a list of the best to worst colour choices by adding together the remaining colour choices of each adjacent node if colour x is chosen. The colour that has the most adjacent colour choices remaining is the best choice. Once the list has been compiled, LeastConstrainingValue will return the list to Backtracking.
+
+Backtracking will then start with the optimal colour choice, assign it to the node, remove that colour option from each of the adjacent nodes, and check if the current colour allocation is legal. If it is, it will then recursively call itself, passing along the current map_array and colour_array
+If backtracking returns an empty list, that means that the current map_array will not lead to a legal map colouring. It resets the current node to an uncoloured state, returns the chosen colour to the list of legal colours for each adjacent node, and tries the next colour in the list from LeastConstrainingValue. If none of the colours in the list result in a legal layout, then Backtracking will return an empty list to its caller.
+
+If every choice of colours results in an illegal layout, then the current map cannot be coloured with the given number of colours.
--- asst2.py
+++ asst2.py
@@ -12,7 +12,7 @@ def selectNextNode(n, k, G, map_array, colour_array):
 	 #chooses the node with the most connections to other unassigned nodes
 	 highest_degree_node = "null"
 	 highest_degree = -1
-	 print("the MRV is", MRV)
+	 #print("the MRV is", MRV)
 	 for i in range(1, n+1): #this for loop will iterate through the whole array, settling on the nodes with the fewest remaining values. It compares each node's adjacency list to find the node with the highest degree
 	 	if (len(colour_array[i]) == MRV) & (map_array[i] == 0): 
 	 		adjacent_node_list = G[i-1] #get the list of all nodes adjacent to the coloured node
@@ -21,36 +21,40 @@ def selectNextNode(n, k, G, map_array, colour_array):
 	 		for j in range(1, len(adjacent_node_list)):
 	 			if map_array[adjacent_node_list[j]] == 0:
 	 				contender_degree = contender_degree + 1 # count the number of adjacent uncoloured nodes
-	 		print("node[", i, "] has a degree of ", contender_degree)
+	 		# print("node[", i, "] has a degree of ", contender_degree)
 	 		if contender_degree > highest_degree: # if the new node has more uncoloured adjacent nodes than the previous highest, then change the highest
 	 			highest_degree = contender_degree
 	 			highest_degree_node = i
 
-	 print("the highest degree node is", highest_degree_node, "with a degree of ", highest_degree)
-	 for i in range(len(colour_array)):
-	 	print(colour_array[i])
+	 #print("the highest degree node is", highest_degree_node, "with a degree of ", highest_degree)
+	 #for i in range(len(colour_array)):
+	 	#print(colour_array[i])
 	 return highest_degree_node
 
 def leastConstrainingValue(n, k, G, map_array, colour_array, node):
-	colour_order = []
+	colour_order = [] #This array will be the list of colours from best to worst choice in Backtracking
 	value_array = [-1]
-	max_value = [-1]
-	for i in range(1, len(colour_array[node])):
+	max_value = [-1] #Value is calculated by assigning a colour to a node and then totaling the number of remaining colour choices for the adjacent nodes. The optimal colour choice has the highest value
+	for i in range(1, len(colour_array[node])): #for each of the available colours for NODE
 		value = 0
-		current_colour = colour_array[node][i]
-		adjacent_node_list = G[node-1]
-		for j in range(1, len(adjacent_node_list)):
-			adjacent_node = adjacent_node_list[j]
-			if (current_colour in colour_array[adjacent_node]):
+		current_colour = colour_array[node][i] 
+		adjacent_node_list = G[node-1] #Get the list of nodes that are connected to NODE
+		for j in range(1, len(adjacent_node_list)): #for each adjacent node
+			adjacent_node = adjacent_node_list[j] 
+			if (current_colour in colour_array[adjacent_node]): #if the adjacent node had current colour as an available choice before, it wouldn't be able to use it, so its value would decrease by one
 				value = value + len(colour_array[adjacent_node]) - 1
-			else:
+			else: #otherwise, its value is the number of remaining colours it has
 				value = value + len(colour_array[adjacent_node])
 
-		print("when using colour", current_colour, "for node", node, "has a value of", value)
+		# print("when using colour", current_colour, "for node", node, "has a value of", value)
 		value_array.append(value)
 		max_value.append(value)
 
-	max_value.sort()	
+	max_value.sort() 
+	#max value is sorted from lowest to highest value. Value_array is still sorted by index, which corresponds to colour choice. Value_array[1] is the value of the first colour from colour_array[NODE].
+	#The following while loop will iterate through value_array, finding the colour with the highest value by comparing it against the last value of max_value. 
+	#Once it is found, it is popped off the end of max_value, and colour_array[NODE][i] is added to colour order as the colour with the highest value.
+	#this process is repeated to create a list of the highest to lowest value by colour choice
 
 	while (len(max_value) > 1):
 		for i in range(1, len(value_array)):
@@ -89,7 +93,7 @@ def backtracking(n, k, G, map_array, colour_array):
 	complete = completeness(n, k, G, map_array)
 	if complete == True:
 		return map_array
-	print("the current map array is:", map_array)
+	#print("the current map array is:", map_array)
 	node = selectNextNode(n, k, G, map_array, colour_array)
 	if node == "null":
 		return []
@@ -123,27 +127,14 @@ def solve(n, k, G):
 	colour_array = [[i for i in range(k + 1)] for j in range(n + 1)] #colour array holds the legal colours left for each node
 	
 	solution = backtracking(n, k, G, map_array, colour_array)
-	return solution
-
-G = [[1,2,3], [2,1,3], [3,1,2], [4,5], [5,4]] # a list of lists
-n = 5 # number of vertices
-k = 2 # number of colours
-#solution = solve(n, k, G)
-#print(solution)
-
-
-G = [[1, 2, 3], [2, 1, 3, 4], [3, 1, 2, 4, 5, 6], [4, 2, 3, 5], [5, 3, 4, 6], [6, 3, 5]]
-n = 6
-k = 3
-map_array = [0, 1, 2, 0, 0, 0, 0]
-colour_array = [[], [0, 3], [0, 3], [0, 3], [0, 1, 3], [0, 1, 2, 3], [0, 1, 2 ,3]]
-node = 4
-#solution = solve(n, k, G)
-#print(solution)
+	answer = []
+	for i in range(1, len(solution)):
+		answer.append((i, solution[i]))
+	return answer
 
 G = [[1, 2, 3, 4, 6, 7, 10], [2, 1, 3, 4, 5, 6], [3, 1, 2], [4, 1, 2], [5, 2, 6], [6, 1, 2, 5, 7, 8], [7, 1, 6, 8, 9, 10], [8, 6, 7, 9], [9, 7, 8, 10], [10, 1, 7, 9]]
 n = 10
-k = 3
+k = 4
 solution = solve(n, k, G)
 print(solution)
 # print("solution is", solution)
